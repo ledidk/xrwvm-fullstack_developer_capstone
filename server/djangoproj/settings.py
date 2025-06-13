083338ab9b16344f7c89a14f59e4aef27f32f714
ledidk <ddkntakirutimana@gmail.com>
2025-06-12 20:24:44 -0400

    editting settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'dealerships.netlify.app',
-]
+ALLOWED_HOSTS = ['*']
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://dealerships-nkvc.onrender.com'
